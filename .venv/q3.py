fa4aaf9f443970054400b1a15ebb52dc418a747c
mylesmorrison <mylesmorrison@me.com>
2025-05-15 17:09:34 +1000

    finished q3.py

--- .venv/q3.py
+++ .venv/q3.py
@@ -63,13 +63,13 @@ def position_mult_index_hash_code(student_id_list):
 
 
 value, length = position_mult_index_hash_code(student_id_list)
-print(f"Hash Code: {value}, m: {HASH_TABLE_SIZE}")
+#print(f"Hash Code: {value}, m: {HASH_TABLE_SIZE}")
 # super simple compression function
 def division_hash_compress(value, m):
     return value % m
 
 hash1 = division_hash_compress(value, HASH_TABLE_SIZE)
-print(f"Compression Value: {hash1}")
+#print(f"Compression Value: {hash1}")
 
 
 
@@ -101,7 +101,7 @@ def polynomial_rolling_hash_code_horners(student_id_list, z=31):
 #print(polynomial_rolling_hash_code_horners(student_id_list))
 
 value, length = polynomial_rolling_hash_code_horners(student_id_list)
-print(f"value: {value}, m: {HASH_TABLE_SIZE}")
+#print(f"value: {value}, m: {HASH_TABLE_SIZE}")
 
 # set values for a and b that are constants the rule is that a and b are not divisible by
 # length so then I should use prime number
@@ -109,7 +109,7 @@ def MAD_hash_compress(value, m, a=31, b=17):
     return (a*value + b) % m
 
 hash2 = MAD_hash_compress(value, HASH_TABLE_SIZE)
-print(f"Compression Value: {hash2}")
+#print(f"Compression Value: {hash2}")
 
 # 3rd Hash Function
 # Folding Hash Code - Hash Code
@@ -129,13 +129,13 @@ def folding_hash_code(key):
     return total, len(key)
 
 value, length = folding_hash_code(student_id_list)
-print(f"value: {value}, m: {HASH_TABLE_SIZE}")
+#print(f"value: {value}, m: {HASH_TABLE_SIZE}")
 
 def universal_hash_compression(value, a=31, b=17, p=10007, m=1003):
     return ((a * value + b) % p) % m
 
 hash3 = universal_hash_compression(value, a=31, b=17, p=10007, m=HASH_TABLE_SIZE)
-print(f"Compression Value: {hash3}")
+#print(f"Compression Value: {hash3}")
 
 
 # one thing to note is that I have made 3 different hash codes and 3 different compression function
@@ -184,7 +184,7 @@ print(f"Compression Value: {hash3}")
 # I think what is interesting as I said before would be to test all the hash functions with compression functions to get
 # a better idea how they work with each other. Another parameter to test would be data sets and the actual size of the
 # hash table that will be used.
-
+'''
 def testing_collisions_from_hash_functions(filename, number):
     file = open(filename)
     student_record = ""
@@ -250,7 +250,7 @@ print(entries)
 print(results1)
 print(results2)
 print(results3)
-
+'''
 
 """
 3.2
@@ -261,6 +261,87 @@ print(results3)
 implemented in Question 3.1. Your hashtable should be able to do insertions and searches (no
 requirement to do deletions).
 """
+
+class DoubleHashTable:
+    def __init__(self, m=57641):
+        self.size = m
+        self.table = [None] * m
+        self.z = 31 # for polynomial rolling hash
+        # for universal hash compression (prime numbers)
+        self.p = 10007
+        self.a1, self.b1 = 31, 29
+        self.a2, self.b2 = 13, 17
+        # for secondary hash compression
+        self.q = 43
+        # for counting collisions
+        self.collisions = 0
+
+    def hashcode(self, key):
+        return polynomial_rolling_hash_code_horners(key, self.z)[0]
+
+    def hash_compress_1(self, key):
+        return universal_hash_compression(key, self.a1, self.b1, self.p, self.size)
+
+    def hash_compress_2(self, key):
+        return 43 - key % 43 # from lectures and need to ensure that 2nd hash compression cannot equal 0.
+
+    def insert(self, key, value):
+        hashcode = self.hashcode(key)
+        index = self.hash_compress_1(hashcode)
+        step = self.hash_compress_2(hashcode) # step can never be 0
+        if step == 0:
+            step = 1
+
+        while self.table[index] is not None and self.table[index][0] != key:
+            index = (index + step) % self.size
+            self.collisions += 1
+
+        self.table[index] = (key, value)
+
+    def search(self, key):
+        hashcode = self.hashcode(key)
+        index = self.hash_compress_1(hashcode)
+        step = self.hash_compress_2(hashcode)
+        i = 0
+
+        while self.table[index] is not None:
+            if self.table[index][0] == key:
+                return self.table[index][1]
+            i += 1
+            index = (index + step) % self.size
+
+        return None  # not found
+
+    def get_student_id_to_test(self, filename, number):
+        file = open(filename)
+        student_record = ""
+        for i in range(number):
+            student_record = file.readline()
+
+        student_record.replace(" ", "")
+        student_id_list = []
+
+        for i in range(8):
+            if student_record[i] == " ":
+                continue
+            student_id_list.append(student_record[i])
+
+        return student_id_list
+
+    def get_student_name(self, filename, number):
+        file = open(filename)
+        student_record = ""
+        for i in range(number):
+            student_record = file.readline()
+
+        student_record.replace(" ", "")
+        student_name = []
+
+        for i in range(9, len(student_record)-1):
+            #print(student_record[i])
+            student_name.append(student_record[i])
+        student_name = "".join(student_name)
+        return student_name
 
 
 
@@ -276,7 +357,69 @@ calculate the average collisions as: (number of collisions/ total number of entr
 hashtable) * 100.
 """
 
+hashTable = DoubleHashTable()
+def report_average_collisions(hashTable, number_of_names):
+    for j in range(1, number_of_names):
+        student_id_list = hashTable.get_student_id_to_test("Student_Records.txt", j)
+        student_name = hashTable.get_student_name("Student_Records.txt", j)
+        hashTable.insert(student_id_list, student_name)
+    return hashTable.collisions / number_of_names
+
+def report_number_of_collisions(hashTable, number_of_names):
+    for j in range(1, number_of_names):
+        student_id_list = hashTable.get_student_id_to_test("Student_Records.txt", j)
+        student_name = hashTable.get_student_name("Student_Records.txt", j)
+        hashTable.insert(student_id_list, student_name)
+    return hashTable.collisions
+
+# (a) 100 names
+collisions = report_average_collisions(hashTable, 100)
+print(f"100 names -> Number of Collisions: {collisions}")
+# (b) 200 names
+collisions = report_average_collisions(hashTable, 200)
+print(f"200 names -> Number of Collisions: {collisions}")
+# (c) 500 names
+collisions = report_average_collisions(hashTable, 500)
+print(f"500 names -> Number of Collisions: {collisions}")
+# (d) entire dataset
+collisions = report_average_collisions(hashTable, 18239)
+print(f"18239 names -> Number of Colllisions {collisions}")
+
+'''
+# plotting
+entries = list(range(100, 10000, 1000))
+collisions = []
+for item in entries:
+    collisions.append(report_average_collisions(hashTable, item))
+
+plt.figure(figsize=(10, 6))
+plt.plot(entries, collisions, label="collisions")
+
+
+plt.xlabel("Number of Data Entries")
+plt.ylabel("Number of Collisions")
+plt.title("Collisions of Hash Table")
+plt.legend()
+plt.grid(True)
+plt.show()
+'''
 """
 3. Explain your results from (2) above.
 """
+
+# one of the issues with the question above is that the student records has a range of 100 to 18239 records this means
+# that for such a large range I need to create a hash table size that is greater than 18239 and this means that there
+# will be significantly more collisions for 18239 names than 100 names. So I think that I needed to test it with a more
+# similar range. As I increase the number of entries into the hash table it seems the number of collisions increases
+# exponentially. This makes sense for a double hashing function as there are more numbers already occupying spaces so
+# there will be more double hashing required continually until the hash index is found.
+
+# from 100 entries it is an average of 0 collisions
+# from 200 entries it is an average of 0 collisions
+# from 500 entries it is an average of 0.018 collisions
+# from 18239 it is an average of 411.584 collisions
+
+# These are the results and when there are many names the hash function seems to be making a lot of collisions and means
+# that another hash function is probably required to make an effective hash function
+
 
