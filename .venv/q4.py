9c20026578e0cb3842a7c46e5a463383a435b979
mylesmorrison <mylesmorrison@me.com>
2025-05-15 17:24:09 +1000

    added q4.py

---
+++ .venv/q4.py
@@ -0,0 +1,21 @@
+"""
+Question 4. 1 (30/100)
+Consider the 6 sorting algorithms you have learnt: Selection Sort, Insertion Sort, Bubble Sort, Merge
+Sort, Heap Sort and Quick Sort. Explain which sorting algorithm(s) is suitable for the three different
+scenarios of input data given below. The data is:
+1. In random order (i.e., you have no information about the order of the elements)
+2. In a nearly sorted order (i.e., only a maximum of 1 or 2 elements are disordered)
+3. In reversed order than your expected sorting order (e.g., if your expected order is: 2, 4, 7, 9, 15
+The given data is in the reversed order as 15, 9, 7, 4, 2)
+You should provide an explanation for the three different scenarios 1, 2, 3 separately (you may take up
+to 150-300 words per scenario.). Your explanation should include, why your chosen sorting algorithm(s)
+is suitable for the given scenario over the other sorting algorithms. You may use this tool to compare the
+different sorting algorithms you have learnt
+"""
+
+#1. Random Order
+
+#2. Nearly Sorted Order
+
+#3. Reverse Order
+
