e578736a5eb47f9ff0e09c37ffc0683de52135c9
mylesmorrison <mylesmorrison@me.com>
2025-05-15 17:35:11 +1000

    added q5.py

--- .venv/q4.py
+++ .venv/q4.py
@@ -19,3 +19,31 @@ different sorting algorithms you have learnt
 
 #3. Reverse Order
 
+"""
+Question 4. 2 (70/100)
+4. a. Implement the Merge sort algorithm in python to sort a list with “K” elements in the descending
+order (decreasing order). You may consider that all “K” elements in the list are integers. You should
+write a function “merge_sort” that takes a python list as a parameter and returns a list that is sorted
+in the descending order. You are free to write other functions to help with your merge sort
+implementation.
+Example of the function behavior:
+Calling the function:
+merge_sort([2, 8, 10, 1, 5, 3, 7, 9, 12, 90])
+Function output:
+[90, 12, 10, 9, 8, 7, 5, 3, 2, 1]
+(30 points)
+b. Draw the Merge sort tree for the above example
+"""
+
+
+"""
+5. Answer the following questions based on the Merge Sort algorithm
+a. Merge sort belongs to the category of Divide and Conquer algorithm design. Explain the Merge
+Sort algorithm with respect to the Divide and Conquer algorithm design paradigm (100 - 250
+words)
+(10 points)
+b. Compare and contrast the Merge sort algorithm with another Divide and Conquer sorting
+algorithm you have learnt (100 – 250 words).
+(10 points)
+
+"""
---
+++ .venv/q5.py
@@ -0,0 +1,40 @@
+"""
+Question 5. 1 (70/100)
+An Euler walk is a traversal of a graph, where each edge is traversed exactly once. Nodes may be visited
+multiple times. Given a non-directed, unweighted graph (G), your task is to output:
+i. Whether a Euler Walk exists for the given graph.
+ii. If a Euler Walk exists, print the nodes in the visited order.
+The graph G will be presented to you in the form of an adjacency list.
+i.e.
+[[1, 2],
+[0],
+[0]]
+Would describe a Graph with 3 nodes and 2 edges. Where nodes are 0, 1, and 2. Edges are 0-1 and 0-2.
+Two Euler walks exist for this graph: 1-0-2, and 2-0-1.
+Your output should be returned from a function as a tuple. Where the first element of the tuple
+indicates if a Euler walk exists or not as True or False and the second element gives the nodes in visited
+order as a list or None (if no Euler walk exist).
+(True, [1, 0, 2]) or (True, [2, 0, 1]) would be valid outputs for the above test case.
+Your program should have a callable function with the name “euler_walk()”. That takes an
+adjacency list (a 2D python list) as the sole input parameter, and a tuple as described above, as the
+return value.
+Example 1
+Calling the function: euler_walk([[1, 2], [0], [0]])
+Function output: (True, [1, 0, 2])
+Example 2
+Calling the function: euler_walk([[1, 2, 3], [0], [0], [0]])
+Function output: (False, None)
+"""
+
+
+"""
+Question 5. 2 (30/100)
+Answer the following questions based on your solution for Question 5.1.
+1. Explain the algorithm you used to decide whether there is a Euler walk or not for the given
+graph? Include a graphical explanation. (150- 200 words)
+(10 points)
+2. Explain the algorithm you used to find the Euler walk, in the case where a valid Euler Walk
+existed. Include a graphical explanation. (150-200 words)
+(20 points)
+
+"""
